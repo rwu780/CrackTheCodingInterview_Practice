16b1b2fd12e6af69564a1e7d943214344158ce1f
James <James@DESKTOP-U2VFS8A>
2018-08-28 19:50:28 -0400

    Simple HashTables class, delegate to HashTable

---
+++ src/chapterVII_technicalQuestions/HashTables.java
@@ -0,0 +1,42 @@
+package chapterVII_technicalQuestions;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class HashTables<T, E> {
+	
+	Hashtable<T, E> ht;
+	
+	public HashTables() {
+		ht = new Hashtable<T, E>();
+	}
+	
+	public void clear() {
+		ht.clear();
+	}
+	
+	public boolean contains(E value) {
+		return ht.contains(value);
+	}
+	
+	public boolean containsKey(T key) {
+		return ht.containsKey(key);
+	}
+	
+	public E put(T key, E value) {
+		return ht.put(key, value);
+	}
+	
+	public E remove(T key) {
+		return ht.remove(key);
+	}
+	
+	public boolean isEmpty() {
+		return ht.isEmpty();
+	}
+	
+	public E replace(T key, E value) {
+		return (E) ht.replace(key, value);
+	}
+
+}
