84b000baaad04926408d6d92a7ddf350e1c494a0
James <James@192.168.0.12>
2018-09-18 21:17:12 -0400

    Create class for intersection and loop detection

---
+++ src/interviewQuestions/chapter2/linkedLists/Intersection.java
@@ -0,0 +1,9 @@
+package interviewQuestions.chapter2.linkedLists;
+
+public class Intersection {
+	/**
+	 * Given two(singly) linked lists. determine if the two lists intersect.
+	 * Return the intersecting node.
+	 */
+
+}
---
+++ src/interviewQuestions/chapter2/linkedLists/LoopDetection.java
@@ -0,0 +1,8 @@
+package interviewQuestions.chapter2.linkedLists;
+
+public class LoopDetection {
+	/**
+	 * Given a circular linked list, implement an algorithm that returns the
+	 * node at the beginning of the loop
+	 */
+}
