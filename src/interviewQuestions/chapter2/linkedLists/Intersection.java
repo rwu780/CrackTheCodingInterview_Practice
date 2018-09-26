dbb53fcbb1d0c8c431221b4a4d5158deef104b6c
Rui.Wu <rui.wu@fdmgroup.com>
2018-09-26 09:02:25 -0400

    Implement loop detection with buffer, however encounter error class cannot be found

--- src/interviewQuestions/chapter2/linkedLists/Intersection.java
+++ src/interviewQuestions/chapter2/linkedLists/Intersection.java
@@ -5,5 +5,9 @@ public class Intersection {
 	 * Given two(singly) linked lists. determine if the two lists intersect.
 	 * Return the intersecting node.
 	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
 
 }
--- src/interviewQuestions/chapter2/linkedLists/LoopDetection.java
+++ src/interviewQuestions/chapter2/linkedLists/LoopDetection.java
@@ -1,8 +1,48 @@
 package interviewQuestions.chapter2.linkedLists;
 
+import java.util.HashMap;
+import java.util.Random;
+
 public class LoopDetection {
 	/**
 	 * Given a circular linked list, implement an algorithm that returns the
 	 * node at the beginning of the loop
 	 */
+	HashMap<Integer, Integer> hashmap;
+	
+	public LoopDetection(){
+		hashmap = new HashMap<Integer, Integer>();
+	}
+	
+	public boolean loopDetectionWithBuffer(Node<Integer> head){
+		
+		while(head != null){
+			if(hashmap.containsKey(head.getValue())){
+				return true;
+			}
+			head = head.getNext();
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		LoopDetection lp = new LoopDetection();
+		
+		Node<Integer> head = new Node<Integer>(0);
+		Node<Integer> next = new Node<Integer>(1);
+		head.setNext(next);
+		Random ran = new Random();
+		
+		for (int i = 2; i < 10; i++) {
+			Node<Integer> newNode = new Node<Integer>(ran.nextInt(10));
+			next.setNext(newNode);
+			next = next.getNext();
+		}
+		head.printNode();
+		System.out.println("Loop Detection: " + lp.loopDetectionWithBuffer(head));
+//		head.printNode();
+	}
+	
+	
+	
 }
